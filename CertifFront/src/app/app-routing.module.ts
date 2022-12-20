dbeff7f34061f96602232797142c9d6817af1ca5
Alizée Quernec <alizee.quernec@hotmail.fr>
2022-12-20 16:43:43 +0100

    preparation route accueil user channels messages

--- CertifFront/src/app/app-routing.module.ts
+++ CertifFront/src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  // Default Route
+  {
+    path: '',
+    redirectTo: 'accueil',
+    pathMatch: 'full'
+  }
+  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
